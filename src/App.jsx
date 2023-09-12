81f3716d7096b39105d192c513670e3939cd7683
[James Alwin Aljecera] <[james.aljecera@awsys-i.com]>
2023-09-12 11:24:35 +0800

    Added Tools and quote page - Alwin

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbars from "./components/Navbars";
 import Hero from "./pages/Hero";
 import Team from "./pages/Team";
 import Services from "./pages/Services";
+import Tools from "./pages/Tools";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Hero />
             <Team />
             <Services />
+            <Tools/>
         </>
     );
 }
---
+++ src/pages/Tools.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Container,Row,Col } from "react-bootstrap";
+import "../css/Tools.css";
+
+const Tools = () => {
+    return (
+        <>
+            <Container fluid className="text-center background">
+                <Row>
+                    <h2 className="mt-4">tools</h2>
+                </Row>
+                <Row xs="auto" className="mt-5 justify-content-md-center">
+                    <Col>
+                        <img src="./JavaScript.png" width={91} height={79}/>
+                    </Col>
+                    <Col> 
+                        <img src="./React.png" width={74} height={72} />
+                    </Col>
+                    <Col>
+                        <img src="./CP4D.png" width={72} height={73}/>
+                    </Col>
+                    <Col>
+                        <img src="./winscp.png" width={77} height={62}/>
+                    </Col>
+                    <Col>
+                        <img src="./DBeaver.png" width={69} height={62}/>
+                    </Col>
+                    <Col>
+                        <img src="./Tableau Logo.png" width={65} height={54}/>
+                    </Col>
+                    <Col>
+                        <img src="./Infosphere.png" width={72} height={47}/>
+                    </Col>
+                    <Col>
+                        <img src="./Teraterm.png" width={51} height={51}/>
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <figure className="text-center pt-5 pb-5">
+                    <blockquote className="blockquote">
+                        <p>"Now if you don't like keyboard shortcuts, firstly, what's wrong with you?"</p>
+                    </blockquote>
+                    <figcaption className="blockquote-footer mt-4">
+                        Jedd Guinto <cite title="Source Title">2023</cite>
+                    </figcaption>
+                </figure>
+            </Container>
+        </>
+    );
+};
+
+export default Tools;
