a85e4d66e7dbc2a3c0b03ddafd3f8971357fed03
[jun] <[jednalig@ssct.edu.ph]>
2023-09-14 10:04:25 +0800

    Fixed view to mobile responsive - Jun

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import "../css/Contacts.css";
 import { Container,Row,Col } from "react-bootstrap";
 
-
-
 const Contacts = () => {
     return (
         <Container fluid className="back-color pb-5" id="contact">
             <Row>
                 <Col className="mt-5">
-                    <Row >
+                    <Row>
                         <Col sm={2}></Col>
                         <Col>
-                            <img src="./AWS Logo.png" className="img-size"/>
+                            <img src="./AWS Logo.png" className="img-size" />
                             <h6 className="text">ADVANCED WORLD SOLUTIONS INC.</h6>
                             <h6 className="text">ADVANCED WORLD SYSTEMS INC.</h6>
                         </Col>
@@ -20,20 +18,20 @@ const Contacts = () => {
                     <Row xs="auto" className="mt-3 justify-content-md-left">
                         <Col sm={2}></Col>
                         <Col>
-                            <img src="./linkedin.png" width={39} height={37}/>
+                            <img src="./linkedin.png" width={39} height={37} />
                         </Col>
                         <Col>
-                            <img src="./facebook.png" width={39} height={39}/>
+                            <img src="./facebook.png" width={39} height={39} />
                         </Col>
                         <Col>
-                            <img src="./github.png" width={36} height={35}/>
+                            <img src="./github.png" width={36} height={35} />
                         </Col>
                     </Row>
                 </Col>
                 <Col className="mt-5">
                     <h4 className="title-text">CONTACT</h4>
                     <p className="p1">Ground Floor, i1 Building, Cebu IT Park, Apas, Cebu City, Philippines</p>
-                    <p className="p2"> Tel: 032 268-6405</p>
+                    <p className="p2">Tel: 032 268-6405</p>
                     <p className="p3">Email: awshr.cebu@awysy-i.com</p>
                     <p className="p2">https://www.awsys-i.com</p>
                 </Col>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -6,32 +6,29 @@ const Services = () => {
     return (
         <Container className="text-center services_layout" id="services">
             <h2 className="text-center">
-                services
+                Services
             </h2>
             <p className="main-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Platea dictumst vestibulum rhoncus est. Elementum sagittis 
-                vitae et leo duis ut. Facilisis gravida neque convallis a cras semper auctor neque vitae. Felis eget nunc 
-                lobortis mattis aliquam faucibus purus. Sit amet tellus cras adipiscing enim eu turpis egestas. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Platea dictumst vestibulum rhoncus est. Elementum sagittis vitae et leo duis ut. Facilisis gravida neque convallis a cras semper auctor neque vitae. Felis eget nunc lobortis mattis aliquam faucibus purus. Sit amet tellus cras adipiscing enim eu turpis egestas.
             </p>
             <Row>
-                <Col>
-                    <img src="./Infosphere.png" className="image-size-1"/>
-                    <h4>etl development</h4>
+                <Col xs={12} sm={6} md={4}>
+                    <img src="./Infosphere.png" className="image-size-1" alt="ETL Development" />
+                    <h4>ETL Development</h4>
                     <p className="sub-service-text">
                         Adipiscing tristique risus nec feugiat in fermentum. Nam at lectus urna duis convallis convallis tellus id.
                     </p>
                 </Col>
-                <Col>
-                    <img src="./Tableau Logo.png" className="image-size-2"/>
-                    <h4>tableau visualization</h4>
+                <Col xs={12} sm={6} md={4}>
+                    <img src="./Tableau Logo.png" className="image-size-2" alt="Tableau Visualization" />
+                    <h4>Tableau Visualization</h4>
                     <p className="sub-service-text">
                         Adipiscing tristique risus nec feugiat in fermentum. Nam at lectus urna duis convallis convallis tellus id.
                     </p>
                 </Col>
-                <Col>
-                    <img src="./Data-Analysis.png" className="image-size-3"/>
-                    <h4>data analysis</h4>
+                <Col xs={12} sm={6} md={4}>
+                    <img src="./Data-Analysis.png" className="image-size-3" alt="Data Analysis" />
+                    <h4>Data Analysis</h4>
                     <p className="sub-service-text">
                         Adipiscing tristique risus nec feugiat in fermentum. Nam at lectus urna duis convallis convallis tellus id.
                     </p>
